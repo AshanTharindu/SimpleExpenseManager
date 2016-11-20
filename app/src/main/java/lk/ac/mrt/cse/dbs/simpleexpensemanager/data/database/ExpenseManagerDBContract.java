2ca8895567220614c7e4087a37268ad6da1586b5
Ashan Tharindu <atharindu94@gmail.com>
2016-11-20 23:10:38 +0530

    final changes added

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/database/ExpenseManagerDBContract.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/database/ExpenseManagerDBContract.java
@@ -12,14 +12,14 @@ public final class ExpenseManagerDBContract {
         public static final String TABLE_NAME = "accounts";
         public static final String COLUMN_NAME_ACCOUNT_NO="account_no";
         public static final String COLUMN_NAME_BANK="bank";
-        public static final String COLUMN_NAME_ACCOUNT_HOLDER="holder";
-        public static final String COLUMN_NAME_INITIAL_BALANCE="balance";
+        public static final String COLUMN_NAME_ACCOUNT_HOLDER="account_holder";
+        public static final String COLUMN_NAME_INITIAL_BALANCE="init_balance";
     }
 
     public static class TransactionTable implements BaseColumns{
         public static final String TABLE_NAME = "transactions";
         public static final String COLUMN_NAME_ACCOUNT_NO="account_no";
-        public static final String COLUMN_NAME_TRANSACTION_TYPE="type";
+        public static final String COLUMN_NAME_TRANSACTION_TYPE="transaction_type";
         public static final String COLUMN_NAME_AMOUNT="amount";
         public static final String COLUMN_NAME_DATE="date";
     }
